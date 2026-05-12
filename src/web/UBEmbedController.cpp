27c57c6f8846341cc382d88c8e13c2cd97dea765
Luigi Arone <larone426@gmail.com>
2026-05-12 20:36:40 -0300

    Add FreeBSD platform support
    FreeBSD already has X11 and most POSIX APIs that the Linux
    implementation uses, so we can share most of the code:
    
    - src/frameworks/CMakeLists.txt and src/gui/CMakeLists.txt: compile
      UBPlatformUtils_linux.cpp and UBKeyboardPalette_linux.cpp on FreeBSD
      as well.
    
    - src/frameworks/UBPlatformUtils_linux.cpp: guard the DBus-based onboard
      keyboard integration with #ifdef Q_OS_FREEBSD, since the onboard
      daemon is not commonly available on FreeBSD.
    
    - src/web/UBEmbedController.cpp and src/podcast/UBPodcastController.cpp:
      add Q_OS_FREEBSD alongside Q_OS_LINUX in the platform ifdefs.
    
    Tested on FreeBSD 15.0-CURRENT amd64 with Qt6 5.15.

--- src/web/UBEmbedController.cpp
+++ src/web/UBEmbedController.cpp
@@ -150,7 +150,7 @@ void UBEmbedController::textChanged(const QString& newText)
     static const QRegularExpression regExp("[<>:\"/\\\\|?*]");
 #endif
 
-#ifdef Q_OS_LINUX // Defined on X11.
+#ifdef defined(Q_OS_LINUX) || defined(Q_OS_FREEBSD) // Defined on X11.
     QString illegalCharList("      < > : \" / \\ | ? * ");
     static const QRegularExpression regExp("[<>:\"/\\\\|?*]");
 #endif
